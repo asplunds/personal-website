9c62b89c09153b46bd97d54754404638efd5f3d4
asplunds <jonathan@asplund.net>
2020-11-13 17:21:36 +0100

    worked on front page

--- client/src/views/index/Index.js
+++ client/src/views/index/Index.js
@@ -16,34 +16,26 @@ export default class Index extends Component {
             <article className="index-page">
                 <div className="constrict">
                     <div>
-                        <h1 className="light-header underline slight-margin">
-                            Undo.club
-                        </h1>
+                        <div>
+                            <h1 className="light-header underline slight-margin">
+                                Hi <span role="img" aria-label="wave">👋</span>
+                            </h1>
+                        </div>
                         <p>
-                            Welcome to my web app
+                            I'm Jonathan Asplund, a 16 year old swedish high school student and part time junior full stack developer. I love creating powerful web applications using the newest technologies.
+                        </p>
+                        <p>
+                            Want to contact me? Don't hesitate to ask anything on my e-mail adress <a href="mailto:jonathan@asplund.net">jonathan@asplund.net</a>
                         </p>
                         <h2 className="light-header underline slight-margin">
-                            Who dis?
+                            How I work
                         </h2>
                         <p>
-                            Hi <span role="img" aria-label="wave">👋</span>,<br /> I'm Undo, best known as a Discord admin. I administer several large Discord communities. When I'm not slamming the gavel I'm probably working on a web developement project. As a fullstack developer I love making rich frontend web applications with powerful, flexible and dynamic backends.
+                            As a developer I always strive to write top quality code which is why I love using technologies such as <a href="https://www.typescriptlang.org/">TypeScript</a>, <a href="https://jsdoc.app/index.html">JSDoc</a> and <a href="https://swagger.io/">Swagger</a>. I believe a good program sits on good code. My style of programming is always looking for the best solution in terms of reusability, flexibility and maintainability. Therefore I believe strongly in a functional approach to programming problems, with this method the application avoids repetetive code and enables reusability. With that said I don't have any problems using an object oriented style, I think     it's important to find the most optimal solution for the task.
                         </p>
                         <p>
-                            ( ͡° ͜ʖ ͡°) Bröther please click below
+                            I'm an active proponent of collaboration in my it's imperative to have good communication with team members in order to produce the best results. Asking and giving help or advice is something I strongly believe in. 
                         </p>
-                        <div>
-                            <Link to="/faq">
-                                <Button
-                                    startIcon={<i className="fad fa-question-square"></i>}
-                                    size="large"
-                                    color="primary"
-                                    variant="contained"
-                                >
-                                    Read FAQ
-                                </Button>
-                            </Link>
-                        </div>
-
                         <Tech />
 
                         {false && <Button
